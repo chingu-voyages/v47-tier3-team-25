002c2e3de65066578f692fb38796072032dc9b73
umesh <umesharma.us11@gmail.com>
2024-01-19 21:56:01 +0530

    setting up next app

---
+++ carease/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'CarEase',
+  description: 'Car Comparision Webapp',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ carease/app/page.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-green-500">CarEase</h1>;
+}
