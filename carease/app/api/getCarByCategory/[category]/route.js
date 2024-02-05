ce12971e0d9d89bbee7c0aa496a7c075e466c015
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-02-05 12:45:28 +0530

    Created an api endpoint for fetching cars by category

---
+++ carease/app/api/getCarByCategory/[category]/route.js
@@ -0,0 +1,15 @@
+import Car from "@/models/car";
+import { NextResponse } from "next/server";
+
+export async function GET(req, content){
+    try{
+        const category = content.params.category
+
+        const carData = await Car.find({ category: category })
+
+        return NextResponse.json(carData, { status : 200 })
+
+    } catch(err){
+        return NextResponse.json({ error : "Internal server error" }, { status : 500 })
+    }
+}
