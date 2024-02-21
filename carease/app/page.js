700b72e2b7d9b82e2455e1296774b5afde71e7e4
umesh <umesharma.us11@gmail.com>
2024-02-21 11:56:33 +0530

    Completed the UI for the footer section

--- carease/app/page.js
+++ carease/app/page.js
@@ -1,9 +1,10 @@
-
+import Footer from '@/Components/Footer';
 
 export default function Home() {
   return (
-    <h1 className="text-green-500">
-      CarEase
-    </h1>
+    <>
+      <h1 className="text-green-500">CarEase</h1>
+      <Footer />
+    </>
   );
 }
