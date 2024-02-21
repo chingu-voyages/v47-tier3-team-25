e247010761bbe7ee3748c57a679f21234693674b
umesh <umesharma.us11@gmail.com>
2024-02-21 13:14:42 +0530

    Completed the UI for the Hero Section of Homepage

--- carease/app/page.js
+++ carease/app/page.js
@@ -1,9 +1,10 @@
 import Footer from '@/Components/Footer';
+import Hero from '@/Components/Hero';
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-green-500">CarEase</h1>
+      <Hero />
       <Footer />
     </>
   );
---
+++ carease/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import HeroImage from '../public/images/Lambo-by-Abdulwahab-Alawadhi.svg';
+import Image from 'next/image';
+import Button from './Button';
+
+const Hero = () => {
+  return (
+    <section className="relative">
+      <div className="h-[724px]">
+        <Image
+          src={HeroImage}
+          width={1440}
+          height={724}
+          alt="Hero"
+          className="w-full h-[724px] object-cover"
+        />
+      </div>
+      <div className="absolute top-[60%] left-10 md:max-w-[50rem] text-white">
+        <h2 className="font-bold text-[30px] md:text-[50px]">AVENTADOR SVJ</h2>
+        <p className="mt-6 text-sm">
+          The Revuelto is the beginning of a new era for Lamborghini, who has
+          harnessed the power of hybridization technology to create the first
+          HPEV (High Performance Electrified Vehicle).
+        </p>
+        <Button
+          styleType={'mt-6 border-2 border-[#D9D9D9]'}
+          name="Explore More"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
